fdeb58b7e55642a04920fc107ede42def9ca26f2
taufik <taufik@codelabs.co.id>
2025-08-22 21:24:21 +0700

    update: wrapper component with template

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,27 @@
-import { AppFooter, AppHeader } from "../components"
+import { Flex } from "antd"
+import { Template } from "../components"
 
 const Dashboard = () => {
   return (
-    <>
-      <AppHeader />
+    <Template isLayout={false}>
+      <Flex
+        gap={20}
+        style={{
+          padding: '8px 24px'
+        }}
+      >
+        <div style={{
+          width: '100%',
+          backgroundColor: 'white',
+          borderRadius: '4px',
+          padding: '24px',
+          // maxHeight: '450px'
+        }}>
 
-      <AppFooter />
-    </>
+        </div>
+      </Flex>
+
+    </Template>
   )
 }
 
